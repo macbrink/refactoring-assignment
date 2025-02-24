bc0cdb89c7ac089f3e7078d858e36c63078798ea
macbrink <marcel@brinkkemper.com>
2025-02-24 19:18:44 +0100

    Add MVC Project

---
+++ refactored-code/Insurify/Insurify.Application/Insurances/GetInsurances/GetInsurancesQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Insurify.Application.Abstractions.Messaging;
+using Insurify.Domain.Insurances;
+
+namespace Insurify.Application.Insurances.GetInsurances;
+
+/// <summary>
+/// Represents a query to get all insurances.
+/// </summary>
+public sealed record GetInsurancesQuery : IQuery<IReadOnlyCollection<Insurance>>;
