64986aaa68ec812a86154979f62f00a07a3fa7f4
macbrink <marcel@brinkkemper.com>
2025-02-24 19:21:22 +0100

    Remove an sort usings

--- refactored-code/Insurify/Insurify.Application/Insurances/GetInsurances/GetInsurancesQuery.cs
+++ refactored-code/Insurify/Insurify.Application/Insurances/GetInsurances/GetInsurancesQuery.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Insurify.Application.Abstractions.Messaging;
+﻿using Insurify.Application.Abstractions.Messaging;
 using Insurify.Domain.Insurances;
 
 namespace Insurify.Application.Insurances.GetInsurances;
