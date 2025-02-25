779f2680925a301ef743cca90387179f6913631a
Marcel Brinkkemper <99471493+macbrink@users.noreply.github.com>
2025-02-25 08:30:11 +0100

    Dependency injection

---
+++ refactored-code/Insurify/Insurify.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Insurify.Application.Abstractions.Behaviors;
+using FluentValidation;
+
+namespace Insurify.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+
+            configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+        return services;
+    }
+}
