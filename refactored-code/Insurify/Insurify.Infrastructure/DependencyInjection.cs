22b1ea23110ae9646611f6324d6d950d01b35759
Marcel Brinkkemper <99471493+macbrink@users.noreply.github.com>
2025-02-25 13:27:44 +0100

    Dependency Injection for Infrastructure.

---
+++ refactored-code/Insurify/Insurify.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Insurify.Application.Abstractions.Data;
+using Insurify.Domain.Abstractions;
+using Insurify.Domain.Customers;
+using Insurify.Domain.InsurancePolicies;
+using Insurify.Domain.Insurances;
+using Insurify.Infrastructure.Data;
+using Insurify.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Insurify.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            if(string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new ArgumentException("Connection string is missing", nameof(connectionString));
+            }
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            services.AddScoped<IInsuranceRepository, InsuranceRepository>();
+
+            services.AddScoped<IInsurancePolicyRepository, InsurancePolicyRepository>();
+
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+            services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+            services.AddSingleton<ISqlConnectionFactory>(_ => new SqlConnectionFactory(connectionString));
+            return services;
+        }
+    }
+}
---
+++ refactored-code/Insurify/Insurify.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Insurify.Domain.Abstractions;
+
+namespace Insurify.Infrastructure
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
