3bd47bf702099e7d253a6b5e174c44c7e5ffe388
macbrink <marcel@brinkkemper.com>
2025-02-27 22:13:08 +0100

    Can't get Migrations Working

--- refactored-code/Insurify/Insurify.Infrastructure/ApplicationDbContext.cs
+++ refactored-code/Insurify/Insurify.Infrastructure/ApplicationDbContext.cs
@@ -3,13 +3,14 @@ using Insurify.Domain.Abstractions;
 using Insurify.Domain.Customers;
 using Insurify.Domain.InsurancePolicies;
 using Insurify.Domain.Insurances;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Insurify.Infrastructure;
 
 public class ApplicationDbContext : DbContext, IUnitOfWork, IApplicationDbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IPublisher publisher)
         : base(options)
     {
     }
@@ -19,4 +20,51 @@ public class ApplicationDbContext : DbContext, IUnitOfWork, IApplicationDbContex
     public DbSet<InsurancePolicy> InsurancePolicies { get; private set;  }
 
     public DbSet<Customer> Customers { get; private set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+
+    /*
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var result = await base.SaveChangesAsync(cancellationToken);
+
+            await PublishDomainEventsAsync();
+
+            return result;
+        }
+        catch(DbUpdateConcurrencyException ex)
+        {
+            throw new ConcurrencyException("Concurrency exception occurred.", ex);
+        }
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                var domainEvents = entity.GetDomainEvents();
+
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        foreach(var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
+    }
+    */
 }
